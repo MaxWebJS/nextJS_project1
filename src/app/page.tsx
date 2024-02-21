d82ae065ad04fc60bd8ed82c8ab6cbb76f239853
Maxim <maxtab1@mail.ru>
2024-02-21 03:08:59 +0300

    initial for checks server side caching...

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,25 @@
-import Image from "next/image";
+// import Image from "next/image";
 
-export default function Home() {
-	return <main>Hello!</main>;
+// export default function Home() {
+// 	return <main>Hello!</main>;
+// }
+
+type Time = {
+	datetime: string;
+};
+
+async function getTime(): Promise<Time> {
+	const res = await fetch(
+		"https://worldtimeapi.org/api/timezone/Europe/Moscow"
+		// { next: { revalidate: 13 } }
+	);
+	return res.json();
 }
+
+const Bits = async ({ params, searchParams }) => {
+	// const [time] = await Promise.all([getTime()]);
+	const [time] = await Promise.all([getTime()]);
+	return <h2>{time.datetime}</h2>;
+};
+
+export default Bits;
