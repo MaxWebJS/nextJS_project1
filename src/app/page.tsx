9704cbb52bfa44cbb14c191ba8d787c86dc7522a
Maxim <maxtab1@mail.ru>
2024-02-23 01:59:40 +0300

    add console.log("Response headers:", res.headers)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,16 +10,24 @@ type Time = {
 
 async function getTime(): Promise<Time> {
 	const res = await fetch(
-		"https://worldtimeapi.org/api/timezone/Europe/Moscow",
-		{ cache: "no-store" }
+		"https://worldtimeapi.org/api/timezone/Europe/Moscow"
+		// { cache: "no-store" }
 		// { next: { revalidate: 13 } }
 	);
-	return res.json();
+	// Log information about caching
+	console.log("Response headers:", res.headers); // Log the response headers including caching information
+
+	const resJSON = res.json();
+	// console.log(typeof res);
+	// console.log(resJSON);
+	// return res.json();
+	return resJSON;
 }
 
 const Bits = async () => {
 	// const [time] = await Promise.all([getTime()]);
 	const [time] = await Promise.all([getTime()]);
+	console.log(time);
 	return <h2>{time.datetime}</h2>;
 };
 
