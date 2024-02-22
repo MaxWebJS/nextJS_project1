a6442c36e665f297e094ae58707672b61d813847
Maxim <maxtab1@mail.ru>
2024-02-23 02:11:11 +0300

    add return <h2>{HeadersList}</h2>;

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,14 +21,15 @@ async function getTime(): Promise<Time> {
 	// console.log(typeof res);
 	// console.log(resJSON);
 	// return res.json();
-	return resJSON;
+	return res.headers;
 }
 
 const Bits = async () => {
 	// const [time] = await Promise.all([getTime()]);
-	const [time] = await Promise.all([getTime()]);
-	console.log(time);
-	return <h2>{time.datetime}</h2>;
+	const [HeadersList] = await Promise.all([getTime()]);
+	// console.log(time);
+	// return <h2>{time.datetime}</h2>;
+	return <h2>{HeadersList}</h2>;
 };
 
 export default Bits;
